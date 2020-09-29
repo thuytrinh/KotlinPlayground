6b2f3408135b75aeb1216a139af437509335c6a6
Thuy Trinh <thuy.trinh@cobi.bike>
2020-09-29 13:38:43 +0200

    Use decodeFromString

--- src/main/kotlin/JsonPlayground.kt
+++ src/main/kotlin/JsonPlayground.kt
@@ -10,12 +10,13 @@ data class Event(
 )
 
 fun main() {
-  val json = Json.encodeToString(
-    Event(
-      title = "Team retro",
-      beginDateTime = "20.08.2020 16:00",
-      endDateTime = "20.08.2020 17:00",
-    )
+  val expected = Event(
+    title = "Team retro",
+    beginDateTime = "20.08.2020 16:00",
+    endDateTime = "20.08.2020 17:00",
   )
+  val json = Json.encodeToString(expected)
+  val actual = Json.decodeFromString(Event.serializer(), json)
   println(json)
+  println(actual)
 }
