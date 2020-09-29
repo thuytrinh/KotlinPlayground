ac3ea3befef96b238f48a78d90f3d7685e3d8bde
Thuy Trinh <thuy.trinh@cobi.bike>
2020-09-29 15:42:02 +0200

    Correct the test

--- src/test/kotlin/CloseChannelTest.kt
+++ src/test/kotlin/CloseChannelTest.kt
@@ -3,6 +3,8 @@ import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
@@ -11,33 +13,40 @@ import kotlin.time.milliseconds
 
 class CloseChannelTest {
   @Test
-  fun `close won't wait till all sent values are processed`() {
+  fun `close should wait till all sent values are processed`() {
     runBlocking {
       // Given
       val channel = BroadcastChannel<Int>(capacity = Channel.BUFFERED)
       val values = mutableListOf<Int>()
+      val waitForCollectToStart = BroadcastChannel<Unit>(capacity = 1)
       val job = launch {
-        channel.asFlow().collect {
-          println("Queuing $it...")
+        println("Launching...")
+        channel.asFlow()
+          .onStart { waitForCollectToStart.send(Unit) }
+          .collect {
+            println("Queuing $it...")
 
-          delay(500.milliseconds)
-          values.add(it)
+            delay(500.milliseconds)
+            values.add(it)
 
-          println("Added $it")
-        }
+            println("Added $it")
+          }
       }
 
       // When
+      println("Waiting for collect()...")
+      waitForCollectToStart.asFlow().first()
+
       (1..10).forEach {
         channel.send(it)
         println("Sent $it")
       }
+
+      println("Closing...")
       channel.close()
-      job.join()
 
       // Then
       assertThat(values).isEmpty()
-      assertThat(job.isCompleted).isTrue()
     }
   }
 }
