391cb45dce20d0764727eb855fabb82914704781
Thuy Trinh <thuy.trinh@cobi.bike>
2020-10-19 11:19:46 +0200

    Add turbine

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,6 @@ repositories {
 dependencies {
   implementation(kotlin("stdlib"))
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.3.9")
   implementation("com.squareup.moshi:moshi-kotlin:1.9.3")
   implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.0-RC2")
   implementation("com.squareup.okio:okio:2.8.0")
@@ -24,6 +23,8 @@ dependencies {
 
   testImplementation("junit", "junit", "4.12")
   testImplementation("org.assertj:assertj-core:3.17.2")
+  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.3.9")
+  testImplementation("app.cash.turbine:turbine:0.2.1")
 }
 
 tasks.withType<KotlinCompile>().all {
