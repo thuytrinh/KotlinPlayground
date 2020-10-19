b64a14cca01fe326f82d6c8d58e48376fdffa0f1
Thuy Trinh <thuy.trinh@cobi.bike>
2020-10-19 12:01:58 +0200

    Use the trick from KotlinLang channel

--- src/test/kotlin/TurbineAndHotFlowTest.kt
+++ src/test/kotlin/TurbineAndHotFlowTest.kt
@@ -8,16 +8,16 @@ class TurbineAndHotFlowTest {
   @Test
   fun `should verify emission of StateFlow`() = runBlockingTest {
     val flow = MutableStateFlow(0)
-    println("Asserting")
     flow.test {
       assertThat(expectItem()).isEqualTo(0)
+
+      flow.value = 1
       assertThat(expectItem()).isEqualTo(1)
+
+      flow.value = 2
       assertThat(expectItem()).isEqualTo(2)
+
       cancelAndIgnoreRemainingEvents()
     }
-    println("Emitting values")
-    flow.value = 1
-    flow.value = 2
-    println("Done")
   }
 }
