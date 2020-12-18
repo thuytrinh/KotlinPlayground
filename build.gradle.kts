16400a384eb229e6482a35b5b47dc5d4ddc0156d
Thuy <thuy.trinh@cobi.bike>
2020-12-18 17:13:08 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   java
-  kotlin("jvm") version "1.4.10"
+  kotlin("jvm") version "1.4.20"
   kotlin("plugin.serialization") version "1.4.10"
 }
 
@@ -15,8 +15,7 @@ repositories {
 }
 
 dependencies {
-  implementation(kotlin("stdlib"))
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
   implementation("com.squareup.moshi:moshi-kotlin:1.9.3")
   implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
   implementation("com.squareup.okio:okio:2.8.0")
@@ -25,8 +24,8 @@ dependencies {
 
   testImplementation("junit", "junit", "4.12")
   testImplementation("org.assertj:assertj-core:3.17.2")
-  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.3.9")
-  testImplementation("app.cash.turbine:turbine:0.2.1")
+  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.4.2")
+  testImplementation("app.cash.turbine:turbine:0.3.0")
 }
 
 tasks.withType<KotlinCompile>().all {
